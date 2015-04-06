223ad02ba9ea72e9fdf0fff94425d5707705fdd8
sparklyballs <sparklyballs@gmail.com>
2015-04-06 06:32:10 +0100

    Update start.sh

--- src/startup-files/start.sh
+++ src/startup-files/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-exec /usr/sbin/urbackup_srv --daemon --plugin /usr/lib/liburbackupserver_urlplugin.so --plugin /usr/lib/liburbackupserver_cryptoplugin.so --plugin /usr/lib/liburbackupserver_fsimageplugin.so --plugin /usr/lib/liburbackupserver.so --http_root /var/urbackup/www --workingdir /var --snapshot_helper /usr/bin/urbackup_snapshot_helper --plugin /usr/lib/liburbackupserver_httpserver.so --port 55413 --logfile /var/log/urbackup.log --loglevel warn --http_port 55414 --pidfile /var/run/urbackup_srv.pid --user root
-
+exec /usr/sbin/start_urbackup_server --no_daemon
